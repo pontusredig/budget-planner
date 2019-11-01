e68d7352033dcf1a94e72e7ba891535593bcc9b4
Pontus Redig <pontusredig@hotmail.com>
2019-11-01 13:34:15 +0100

    Initial commit

---
+++ src/main/java/se/yrgo/budgetplanner/BudgetplannerApplication.java
@@ -0,0 +1,13 @@
+package se.yrgo.budgetplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetplannerApplication.class, args);
+    }
+
+}
