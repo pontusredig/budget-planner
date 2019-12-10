e893e5833b8f212c5357cb24376c35fdb86b48cc
Pontus Redig <pontusredig@hotmail.com>
2019-12-10 12:26:26 +0100

    Switching to RemoteMySQL database

--- src/main/java/se/yrgo/budgetplanner/configuration/WebConfiguration.java
+++ src/main/java/se/yrgo/budgetplanner/configuration/WebConfiguration.java
@@ -1,16 +1,12 @@
 package se.yrgo.budgetplanner.configuration;
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfiguration {
-    @Bean
-    ServletRegistrationBean h2servletRegistration() {
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-        registrationBean.addUrlMappings("/console/*");
-        return registrationBean;
-    }
-    }
+//import org.h2.server.web.WebServlet;
+//@Configuration
+//public class WebConfiguration {
+//    @Bean
+//    ServletRegistrationBean h2servletRegistration() {
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+//        registrationBean.addUrlMappings("/console/*");
+//        return registrationBean;
+//    }
+//    }
